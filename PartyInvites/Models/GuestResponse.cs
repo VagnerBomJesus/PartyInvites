f4548993e096e71d02f578e9af655fb45224c854
Vagner Bom Jesus <vagneripg@gmail.com>
2020-11-12 09:50:38 +0000

    Criação do Patryinvites

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -9,12 +9,15 @@ namespace PartyInvites.Models
     public class GuestResponse
     {
         [Required(ErrorMessage = "Please enter your name")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage ="The name Should have at least 2 chares anda a maxim 80")]//aceita maximo 80 carateries e no minimo 2 carater
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter your phone")]
         public string Phone { get; set; }
 
         [Required(ErrorMessage = "Please enter a valid email")]
+        
+        [EmailAddress(ErrorMessage = "Please enter a valide email")]
         public string Email { get; set; }
 
         public bool? WillAttend { get; set; }
